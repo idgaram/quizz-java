7d9f78ad5ce95b17854671cc0108fa00c12cc776
IDgaram <lorys.lellouche@gmail.com>
2025-03-06 15:02:55 -0500

    first commit

---
+++ QuestionService.java
@@ -0,0 +1,23 @@
+public class QuestionService {
+
+   Question[] questions = new Question[5];
+
+   public QuestionService(){
+    questions[0] = new Question(1, "quel est nom de moi", "lorys", "leuri", "daniel", "antoine", "lorys");
+    questions[1] = new Question(2, "quel est couleur de moi", "lorys", "leuri", "jaune", "antoine", "jaune");
+    questions[2] = new Question(3, "quel est age de moi", "28", "leuri", "daniel", "antoine", "28");
+    questions[3] = new Question(4, "quel est taille de moi", "lorys", "leuri", "daniel", "1m85", "1m85");
+    questions[4] = new Question(5, "quel est sens de la vie", "plaisir", "leuri", "daniel", "antoine", "lorys");
+
+    
+   }
+   
+   public void displayQuestions(){
+
+for (Question q : questions) {
+   System.out.println(q.getQuestion());
+}
+   //  System.out.println(questions[0].getQuestion());
+   }
+    
+}
